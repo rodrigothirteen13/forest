b6daf34f8f723c13a4d0120d6d72213c011eed1b
Rodrigo_Gonçalves <rodrigoncalves.net@outlook.com>
2025-02-07 11:46:50 -0300

    data e clima finalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html, js}'],
+  content: ["./src/**/*.{html, js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "0.5rem",
+        sm: "1rem",
+      },
+    },
+    extend: {
+      colors: {
+        verde: {
+          200: "#ACEF75",
+          300: "#91EE77",
+          400: "#17E880",
+          700: "#2E482C",
+          800: "#16281F",
+          900: "#0F1C15",
+          950: "#030504",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
